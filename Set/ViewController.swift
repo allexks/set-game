094fd6d9dbdb82490a81004dc10d8ca0e8ade824
Aleksandar Ignatov <aleksandar@mentormate.com>
2019-05-21 12:44:26 +0300

    Initial Commit

---
+++ Set/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Aleksandar Ignatov on 21.05.19.
+//  Copyright © 2019 MentorMate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
